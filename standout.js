b838602c762828c8eb5c73642f2c15f286e54633
Donnie <donniepask@gmail.com>
2020-06-14 20:34:42 +0200

    Fixed multiple elements behaviour
    Optimized code
    Fixed bug for multiple elements in viewport at same time
    Fixed lightbox effect for multiple elements in viewport at same time

--- standout.js
+++ standout.js
@@ -2,7 +2,6 @@
     $.fn.standout = function(options){
         // This is the easiest way to have default options.
         let settings = options.lightBoxEffect ? $.extend({}, $.fn.standout.defaults, options, $.fn.standout.defaultMethods) : $.extend({}, $.fn.standout.defaults, options);
-        // let obj = $(this);
         let i = 0;
         let init = false;
 
@@ -12,66 +11,57 @@
 
         return this.each(function(){
             let obj = $(this);
-            let id = obj.attr("id") + "_clone_" + i;
+            let id = "standout_clone_" + i;
+            obj.objProps = $.extend({}, $.fn.standout.data);
             obj.customId = id;
-            if(settings.lightBoxEffect) {
-                if(!init) {
-                    overlayElement(settings);
-                    init = true;
-                }
-                duplicateElement(obj, settings, i);
-            }
+            init = initialize(obj, settings, i, init);
             i++;
 
             $(window).on("resize scroll", function(){
-                obj.objProps = $.fn.standout.data;
-                objProps = obj.objProps;
-                objProps.init(obj).update(obj, settings);
-                if(objProps.isInViewport()) {
-                    obj.status = objProps.status();
-                    if((!settings.onlyFirstTime && obj.status === objProps.lastEvent) || obj.status != objProps.lastEvent) {
-                        switch(obj.status) {
+                obj.objProps.init(obj).update(obj, settings);
+                if(obj.objProps.isInViewport()) {
+                    obj.objProps.currentEvent = obj.objProps.status();
+                    if((!settings.onlyFirstTime && obj.objProps.currentEvent === obj.objProps.lastEvent) || obj.objProps.currentEvent != obj.objProps.lastEvent) {
+                        switch(obj.objProps.currentEvent) {
                             case "EB":
-                                settings.enteringFromBottom(obj,objProps);
+                                settings.enteringFromBottom(obj);
                                 break;
                             case "EXB":
-                                settings.exitingFromBottom(obj,objProps);
+                                settings.exitingFromBottom(obj);
                                 break;
                             case "ET":
-                                settings.enteringFromTop(obj,objProps);
+                                settings.enteringFromTop(obj);
                                 break;
                             case "EXT":
-                                settings.exitingFromTop(obj,objProps);
+                                settings.exitingFromTop(obj);
                                 break;
                             case "C":
-                                settings.center(obj,objProps);
+                                settings.center(obj);
                                 break;
                             case "O":
-                                settings.over(obj,objProps);
+                                settings.over(obj);
                                 break;
                             case "U":
-                                settings.under(obj,objProps);
+                                settings.under(obj);
                                 break;
                             default:
                                 break;
                         }
                     }
-                    objProps.lastEvent = obj.status;
+                    obj.objProps.lastEvent = obj.objProps.currentEvent;
                 }
             });
         });
-
-        // return this;
     };
 
     $.fn.standout.defaultMethods = {
-        enteringFromTop: function(obj,opt){return opt.fading(obj);},
-        exitingFromTop: function(obj,opt){return opt.fading(obj);},
-        center: function(obj,opt){opt.showing(obj);},
-        enteringFromBottom: function(obj,opt){return opt.fading(obj);},
-        exitingFromBottom: function(obj,opt){return opt.fading(obj);},
-        under: function(obj,opt){opt.hiding(obj);},
-        over: function(obj,opt){opt.hiding(obj);},
+        enteringFromTop: function(obj){return obj.objProps.fading(obj);},
+        exitingFromTop: function(obj){return obj.objProps.fading(obj);},
+        center: function(obj){return obj.objProps.showing(obj);},
+        enteringFromBottom: function(obj){return obj.objProps.fading(obj);},
+        exitingFromBottom: function(obj){return obj.objProps.fading(obj);},
+        under: function(obj){obj.objProps.fading(obj);},
+        over: function(obj){obj.objProps.fading(obj);},
         onlyFirstTime: false
     }
 
@@ -152,6 +142,7 @@
         viewportBottom: 0,
         lastViewportTop: 0,
         lastEvent: "",
+        currentEvent: "",
         isInViewport: function() {
             return this.elementBottom > this.viewportTop && this.elementTop < this.viewportBottom && this.initialized;
         },
@@ -222,12 +213,11 @@
             return this;
         },
         fading: function(obj) {
-            // Bug for lightbox effect when multiple standout elements are in the viewport at same time
             $("#overlayStandout").css({
                 "display": "block",
                 "opacity": this.percentage() < 0.75 ? this.percentage() : 0.75
             });
-            $("#"+obj.customId).css({
+            $("."+obj.customId).css({
                 "display": "block",
                 "position": "absolute",
                 "top": this.originalTop,
@@ -240,12 +230,11 @@
             return obj;
         },
         showing: function(obj){
-            // Bug for lightbox effect when multiple standout elements are in the viewport at same time
             $("#overlayStandout").css({
                 "display": "block",
                 "opacity": 0.75
             });
-            $("#"+obj.customId).css({
+            $("."+obj.customId).css({
                 "display": "block",
                 "position": "absolute",
                 "top": this.originalTop,
@@ -258,12 +247,20 @@
             return obj;
         },
         hiding: function(obj) {
-            // Bug for lightbox effect when multiple standout elements are in the viewport at same time
-            $("#overlayStandout").fadeOut();
-            $("#"+obj.customId).fadeOut();
             return obj;
         }
     };
+
+    function initialize(obj, settings, i, init) {
+        if(settings.lightBoxEffect) {
+            if(!init) {
+                overlayElement(settings);
+                init = true;
+            }
+            duplicateElement(obj, settings, i);
+        }
+        return init;
+    }
 
     function overlayElement(opt) {
         let overlay = $("<div />").css(opt.overlay).attr("id", "overlayStandout");
@@ -277,8 +274,8 @@
     }
 
     function getOuterHtml(el, i) {
-        let id = el.attr("id") + "_clone_" + i;
-        return $('<div />').append(el.eq(0).clone().css("display", "none").attr("id", id)).html();
+        let id = "standout_clone_" + i;
+        return $('<div />').append(el.eq(0).clone().css("display", "none").addClass(id)).html();
     }
 
     function demoLayout(opt) {
